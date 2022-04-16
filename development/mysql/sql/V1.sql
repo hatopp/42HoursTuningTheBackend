8b2740801937c8311248dc1c3be56425a4832bc6
hatopopo <94654563+hatopp@users.noreply.github.com>
2022-04-16 22:36:56 +0900

    index_21

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -10,6 +10,19 @@ alter table group_member add index i9(user_id)
 ALTER TABLE category_group ADD INDEX i10(group_id);
 ALTER TABLE record ADD INDEX i11(status);
 ALTER TABLE record_comment ADD INDEX i12(linked_record_id);
+ALTER TABLE record_last ADD INDEX i13(user_id);
+ALTER TABLE record_last ADD INDEX i14(record_id);
+ALTER TABLE record ADD INDEX i15(status,updated_at,record_id);
+
+ALTER TABLE record ADD INDEX i16(created_by,status,updated_at,record_id);
+
+ALTER TABLE record_last_access ADD INDEX i17(user_id,record_id);
+ALTER TABLE record ADD INDEX i18(created_by,status);
+ALTER TABLE record ADD INDEX i19(status,record_id);
+
+ALTER TABLE record_comment ADD INDEX i20(linked_record_id,created_at);
+
+ALTER TABLE group_member ADD INDEX i21(user_id,is_primary);
 
 
 alter table record_item_file add index i7(linked_record_id, item_id);
